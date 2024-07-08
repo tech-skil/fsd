e3c541ecc09ad1273901057a35e1a7d123ec6a66
Jayanth <jayanthjay295@gmail.com>
2024-07-08 15:31:14 +0530

    ct

--- Rootproject/app/models.py
+++ Rootproject/app/models.py
@@ -4,6 +4,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     enrollment_date = models.DateField()
+    description = models.TextField(max_length=500 , blank=True)
 
     def __str__(self):
         return self.name
